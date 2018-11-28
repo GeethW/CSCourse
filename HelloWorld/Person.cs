3e0b097cac2bd20af3255eaa8f2cfb9926a4de38
Geethma <geethma.wijenayake@yahoo.com>
2018-11-28 12:03:43 +0800

    lecture 28

---
+++ HelloWorld/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        public Person()
+        {
+            firstName = "";
+            lastName = "";
+        }
+
+        public void introduce()
+        {
+            Console.WriteLine("Hi, I'm " + firstName + " " + lastName);
+        }
+    }
+
+    public class Prog 
+    {
+        public static void Main(String[] args)
+        {
+            Person p = new Person();
+            p.firstName = "Lia";
+            p.lastName = "Lop";
+            p.introduce();
+        }
+    }
+}
