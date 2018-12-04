50d10e02f92c60fa4a945b866e1fe85b3c366b76
GeethW <c32799545@uon.edu.au>
2018-12-04 12:21:57 +0800

    cont

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -13,6 +13,10 @@ namespace HelloWorld
             builder.Append("Header");
             builder.AppendLine();
             builder.Append('-', 10);
+            builder.Replace('-', '+');
+            builder.Remove(0, 10);
+            builder.Insert(0, new string('-', 10));
+
             Console.WriteLine(builder);
         }
     }
