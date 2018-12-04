bfc6bb9a66157d186907a64deca1525ff60de2dd
GeethW <c32799545@uon.edu.au>
2018-12-04 12:40:34 +0800

    L68 exercises

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,16 +8,42 @@ namespace HelloWorld
         public static void Main(string[] args)
         {
             var builder = new StringBuilder();
-            builder.Append('-', 10);
-            builder.AppendLine();
-            builder.Append("Header");
-            builder.AppendLine();
-            builder.Append('-', 10);
-            builder.Replace('-', '+');
-            builder.Remove(0, 10);
-            builder.Insert(0, new string('-', 10));
+               builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10);
+                .Replace('-', '+')
+                .Remove(0, 10)
+                .Insert(0, new string('-', 10));
 
             Console.WriteLine(builder);
+            Console.WriteLine("First char: " + builder[0]);
+            task1();
+        }
+
+        public static void task1()
+        {
+            Console.WriteLine("Enter a series of numbers sperated by '-'.");
+            var input = Console.ReadLine();
+            var nums = input.Split('-');
+            var num = Convert.ToInt32(nums[0]);
+            for (int i = 1; i<nums.Length; i++)
+            {
+                var num2 = Convert.ToInt32(nums[i]);
+                if(num>num2)
+                {
+                    Console.WriteLine("Not consecutive.");
+                    return;
+                }
+            }
+            Console.WriteLine("Consecutive.");
+        }
+
+        public static void task2()
+        {
+
         }
     }
 }
